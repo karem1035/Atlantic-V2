62c568e842615293e1528c32a58c56268dfd28b7
Karem <karemdarwesh1035@gmail.com>
2023-12-20 12:00:21 +0200

    finish project

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,42 +1,42 @@
-window.addEventListener("scroll", scrollFunction);
+window.addEventListener('scroll', scrollFunction);
 
 function scrollFunction() {
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  var navbar = document.getElementById("navbar");
-  var logo = document.getElementById("logo");
-  var topbar = document.getElementById("topbar");
+  var navbar = document.getElementById('navbar');
+  var logo = document.getElementById('logo');
+  var topbar = document.getElementById('topbar');
 
   if (scrollTop >= 20) {
-    navbar.style.padding = "20px 0";
-    logo.style.width = "50%";
-    topbar.style.display = "none";
+    navbar.style.padding = '20px 0';
+    logo.style.width = '50%';
+    topbar.style.display = 'none';
   } else {
-    navbar.style.padding = "10px 0 20px 0";
-    logo.style.width = "80%";
-    topbar.style.display = "block";
+    navbar.style.padding = '10px 0 20px 0';
+    logo.style.width = '80%';
+    topbar.style.display = 'block';
   }
 }
 
 // Add CSS transition to the 'navbar' and 'logo' elements
-document.getElementById("navbar").style.transition = "padding 0.5s ease";
-document.getElementById("logo").style.transition = "width 0.5s ease";
+document.getElementById('navbar').style.transition = 'padding 0.5s ease';
+document.getElementById('logo').style.transition = 'width 0.5s ease';
 
-document.querySelectorAll(".navbar-nav a").forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    document.querySelector(".navbar-toggler").click(); // Simulate a click on the toggle button
+document.querySelectorAll('.navbar-nav a').forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    document.querySelector('.navbar-toggler').click(); // Simulate a click on the toggle button
   });
 });
 
 // Change the images
 
 function updateCarouselImages() {
-  var carouselItems = document.querySelectorAll("#mainSlider .carousel-item");
+  var carouselItems = document.querySelectorAll('#mainSlider .carousel-item');
 
   carouselItems.forEach(function (item, index) {
     var mobileImagePath = `assests/images/slider/slider-${index + 1}-mob.jpg`;
     var largeImagePath = `assests/images/slider/slider-${index + 1}.jpg`;
 
-    var imageElement = item.querySelector("img");
+    var imageElement = item.querySelector('img');
 
     if (window.innerWidth < 769) {
       imageElement.src = mobileImagePath;
@@ -47,35 +47,17 @@ function updateCarouselImages() {
 }
 
 // Call the function on page load and resize
-window.addEventListener("load", updateCarouselImages);
-window.addEventListener("resize", updateCarouselImages);
+window.addEventListener('load', updateCarouselImages);
+window.addEventListener('resize', updateCarouselImages);
 
 // Check if the screen width is 1050px or more
-if (window.innerWidth >= 1050) {
-  // Select all img elements
-  const images = document.querySelector(".carousel-item");
-  // Add event listeners to each img element
-  images.forEach((image) => {
-    image.addEventListener("mouseover", (event) => {
-      // Add the class to the hovered img element
-      event.target.classList.add("example");
-      $(".example").extm({
-        zoomElement: true,
-        imageSrc: $(this).attr("src"),
-        squareOverlay: false,
-        position: "right",
-        rightPad: 0,
-        lazy: false,
-        zoomLevel: 2,
-        zoomSize: false,
-        loadingText: false,
-        loadingImage: false,
-      });
-    });
 
-    image.addEventListener("mouseout", (event) => {
-      // Remove the class from the unhovered img element
-      event.target.classList.remove("example");
+$(document).ready(function () {
+  if (window.innerWidth >= 1000) {
+    $('.zoom').magnify({
+      speed: 100,
+      magnifiedWidth: 700,
+      magnifiedHeight: 700,
     });
-  });
-}
+  }
+});
